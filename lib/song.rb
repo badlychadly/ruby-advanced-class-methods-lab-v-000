0aa5dd58184bb4fc692f947f90297309723a6135
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-15 00:31:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,7 @@ class Song
   end
 
   def self.create_from_filename(filename)
-  self.new_from_filename(filename).create
+  self.create = self.new_from_filename(filename)
   end
 
   def self.destroy_all
